56d2bea56a1e9cc63ad468fa0a2cce21097c588a
blair <blair@tentwentyfour.ca>
2014-09-25 21:09:01 -0600

    namespace error fixed

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ten24\PagesBundle\DependencyInjection;
+namespace Ten24\CMSPagesBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('ten24_pages');
+        $treeBuilder->root('ten24_cms_pages');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
